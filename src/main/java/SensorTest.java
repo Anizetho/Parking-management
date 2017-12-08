e7a939ce7ff124caf063820c96b24f39a3aa3bdd
sellto <selleslagh.tom@gmail.com>
2017-12-08 15:53:28 +0100

    Add SensorTest

---
+++ src/main/java/SensorTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SensorTest {
+    @Test
+    void addObserver() {
+        assertEquals(false,false);
+    }
+
+    @Test
+    void removeObserver() {
+        assertEquals(false,false);
+    }
+
+    @Test
+    void notifyObservers() {
+        assertEquals(false,false);
+    }
+
+    @Test
+    void getState() {
+        assertEquals(false,false);
+    }
+
+    @Test
+    void getID() {
+        assertEquals(false,false);
+    }
+
+    @Test
+    void setState() {
+        assertEquals(false,false);
+    }
+
+}
