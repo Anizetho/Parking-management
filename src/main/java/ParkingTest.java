59c6aba937a6a2463f325eec8ac998abc310da90
Albert Emile <ebh.albert@gmail.com>
2017-12-15 14:23:27 +0100

    Add test classes

--- src/main/java/ParkingTest.java
+++ src/main/java/ParkingTest.java
@@ -1,5 +1,37 @@
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingTest {
+    // Create sensor object
+    // Create parking object
+
+    @Test
+    void addSensor() {
+        // Check that if when addSensor is used the list sensors is incremented
+        // Check that the name of the sensor added is containing in the sensor list
+
+        assertEquals(false,false);
+    }
+    @Test
+    void removeSensor() {
+        // Check that if when removeSensor is used the list sensors is decremented
+        // Check that the name of the sensor deleted is no more containing in the sensor list
+        assertEquals(false,false);
+    }
+
+    @Test
+    void addZone() {
+        assertEquals(false,false);
+    }
+
+    @Test
+    void removeZone() {
+        assertEquals(false,false);
+    }
+
+    @Test
+     void getSensorState() {
+        assertEquals(false,false);
+    }
 
 }
