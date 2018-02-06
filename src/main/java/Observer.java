4c9017f4327e4291486935d3b78f3c6c1d08d608
Selleslagh Tom <selltom@iMac-de-Selleslagh.local>
2018-02-06 15:46:43 +0100

    ponctuation

--- src/main/java/Observer.java
+++ src/main/java/Observer.java
@@ -1,9 +1,9 @@
 /**
- *
+ *Design Pattern abstract.
  */
 public abstract class Observer {
     /**
-     *
+     *@param state is the state from the notifyObservers method.
      */
     public abstract void update(boolean state);
 }
