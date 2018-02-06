f469f52b5de700bccc40324a4d78fb1721e61e4d
Selleslagh Tom <selltom@iMac-de-Selleslagh.local>
2018-02-06 15:42:38 +0100

    Observable fix

--- src/main/java/Observable.java
+++ src/main/java/Observable.java
@@ -1,17 +1,19 @@
 /**
- *
+ *Design Pattern abstract
  */
 public abstract class Observable {
     /**
-     *
+     *Add observer into a Observable object
+     *@param obs is the observer
      */
     public abstract void addObserver(Observer obs);
     /**
-     *
+     *Remove observer from a Observable object
+     *@param obs is the observer
      */
     public abstract void removeObserver(Observer obs);
     /**
-     *
+     *Notify observer throw update function
      */
     public abstract void notifyObservers();
 }
