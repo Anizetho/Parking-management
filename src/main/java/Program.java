a4aade1e8edf6ad0d6fdffdb8e0aa56ce8bcebff
Selleslagh Tom <selltom@iMac-de-Selleslagh.local>
2018-02-07 11:22:12 +0100

    checkstyle fix

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -6,15 +6,36 @@ import java.util.HashMap;
 import java.util.List;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import static java.lang.System.out;
 
-
-
+/**
+ *Program is the main class.
+ */
 public class Program {
     /**
      *Cmd is a enum which contain all of the available command.
      */
     enum Cmd {
-        show, change, sensor, free, exit
+        /**
+         *Show the state of the parking.
+         */
+        show,
+        /**
+         *Change the state of a specific sensor.
+         */
+        change,
+        /**
+         *show the state of a specific sensor.
+         */
+        sensor,
+        /**
+         *Show the free place zone by zone.
+         */
+        free,
+        /**
+         *shutdown the simulator.
+         */
+        exit
     }
     /**
      *myParking is the car park managed by the program.
@@ -24,6 +45,7 @@ public class Program {
 
     /**
      *method used to simulate parking activity.
+     * @return is the boolean that stop super loop, or not.
      */
     private static boolean simulator() {
         System.out.print("ParkingCMD > ");
@@ -44,11 +66,11 @@ public class Program {
                     getMyParking().getSensors().get(arg).switchState();
                     break;
                 case sensor:
-                    System.out.println(getMyParking().getSensorState(arg));
+                    out.println(getMyParking().getSensorState(arg));
                     break;
                 case free:
                     for (Zone zone : getMyParking().getZones()) {
-                        System.out.println(zone.getId() + " " + zone.getFreeSpace());
+                        out.println(zone.getId() + " " + zone.getFreeSpace());
                     }
                     break;
                 case exit:
@@ -64,28 +86,29 @@ public class Program {
 
     /**
      *method used to parse the database.
-     * the database is store into a json file
-     * and converted into a Hash map.
+     * @param filename is the path of a json file.
+     * @return is a Hash map.
      */
-    private static HashMap<String, List<Integer>> jsonToHashMap(final String filename) {
+    private static HashMap<String, List<Integer>> j2Hm(final String filename) {
         try {
             InputStream input = new FileInputStream(filename);
             try {
                 return new ObjectMapper().readValue(input, HashMap.class);
             } catch (IOException b) {
-                System.out.println("Json to hash map conversion Failed");
+                out.println("Json to hash map conversion Failed");
             }
         } catch (FileNotFoundException e) {
-            System.out.println("Json File Not Found");
+            out.println("Json File Not Found");
         }
         return new HashMap<String, List<Integer>>();
     }
 
     /**
      *method used to create the different objects present into the car park.
+     * @return the built parking.
      */
     private static Parking init() {
-        HashMap<String, List<Integer>> db = jsonToHashMap("src/Database.json");
+        HashMap<String, List<Integer>> db = j2Hm("src/Database.json");
         Parking park = new Parking();
         for (HashMap.Entry<String, List<Integer>> entry : db.entrySet()) {
             park.addZone(entry.getKey());
@@ -107,6 +130,7 @@ public class Program {
     /**
      *Main program.
      *(Super Loop model)
+     * @param args are the arguments passed to the program at launch
      */
     public static void main(final String[] args) {
         myParking = init();
